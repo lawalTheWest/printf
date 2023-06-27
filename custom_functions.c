d8f99fbbd6598590d99070bc620068f372ed9295
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-06-28 00:41:06 +0100

    custom functions

--- custom_functions.c
+++ custom_functions.c
@@ -10,10 +10,10 @@ int _strlen(const char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
-	{
+	/* while (s[i] != '\0') */
+	do {
 		i++;
-	} /* end while */
+	} while (s[i] != '\0'); /* end while */
 	return (i);
 } /* end of function */
 
